c5891ecb8b32016ba0e2449c76251a3259c89881
Johnathan Asbee <johnathanasbee@gmail.com>
2016-08-29 16:57:45 -0400

    corrected some minor errors
    1. Added an extra line clear due to formatting.
    2. corrected my logic for the pause and reset functions

--- timer.py
+++ timer.py
@@ -2,6 +2,8 @@ import time
 import msvcrt
 
 x = input('Press s to start, p to pause, or r to reset: ')
+pause1 = 0.0
+pause2 = 0.0
 
 if x is 's':
     start = time.clock()
@@ -9,18 +11,22 @@ if x is 's':
 
     while True:
         if x is 'p':
+            pause1 = time.clock() + pause1
             x = msvcrt.getwch()
+            pause2 = time.clock() + pause2
             x1 = 'p'
         elif x is 'r':
-            start = time.clock()
             print('                         ', end='\r')
             print('0:0:0.00', end='\r')
             x = msvcrt.getwch()
+            start = time.clock()
+            pause1 = 0.0
+            pause2 = 0.0
         elif x is 's':
             if msvcrt.kbhit():
                 x = msvcrt.getwch()
             else:
-                current = time.clock() - start
+                current = time.clock() - (start + (pause2 - pause1))
                 minutes = current / 60
                 seconds = current % 60
                 hours = minutes / 60
@@ -28,6 +34,8 @@ if x is 's':
                 a = str(int(hours))
                 b = str(int(minutes))
                 c = str(round(seconds, 2))
+                if float(c) < 0.02:
+                    print('                         ', end='\r')
                 print(a + ':' + b + ':' + c, end='\r')
                 x1 = 's'
         else:
